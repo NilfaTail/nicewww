6820f9575abf317fe41907091d059778f44a866e
Nilfa Tail <nilfa.tail@gmail.com>
2019-01-04 01:15:20 +0300

    страничка с GUI и фикс меню слева, доп.навигация

--- js/main.js
+++ js/main.js
@@ -39,4 +39,27 @@ $(document).ready(function() {
 		});
 	});
 
+	// скрипт прокрутки для бокового меню
+
+	var mh_hght = 195; // высота дива с приветствием и логотипом
+	var mh_mrg = 85; // высота отступа, когда шапка не видна
+
+	var mtop = $(this).scrollTop(); // расстояние до верха
+	var mnavTop = $('.rightNavigation'); // топ-меню	
+
+	// до скрола, если страница загрузилась на середине контента
+	if (mtop - mh_hght >= 0) {
+		mnavTop.css('top', mh_mrg);
+	}
+	
+	// скролим
+	$(window).scroll(function() {  
+		mtop = $(this).scrollTop();
+		if (mtop+mh_mrg < mh_hght) {
+			mnavTop.css('top',(mh_hght - mtop))
+		} else {
+			mnavTop.css('top', mh_mrg)  // устанавливаем значение отступа
+		}
+	});
+
 });
