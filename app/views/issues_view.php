88c1a8051acc680504f147492e04ff28146bd1a8
Nilfa Tail <nilfa.tail@gmail.com>
2019-02-24 18:45:01 +0300

    задачки

--- app/views/issues_view.php
+++ app/views/issues_view.php
@@ -2,8 +2,104 @@
 	<h1>Задачки</h1>
 </div>
 
+<div>
+	<script>
+	/*setTimeout(reminder, 5000);
+	function reminder() {
+			alert("Так и будешь смотреть на эту скучную страницу?");
+	}
+	window.onload = function() {
+		alert('Документ загружен');
+	}*/
+	</script>
+</div>
+
+<div class="article">
+	<h2>Валидация телефона и email (js)</h2>
+	<div>
+		
+	<form action="" method="" id="testPhoneEmail">
+		<div class="form-group">
+			<input type="phone" name="phone" placeholder="79262146670" pattern="[0-9]{2}" title="Формат: 22" required>	
+		</div>
+		<div class="form-group">
+			<input type="email" name="email" placeholder="test@mail.ru" required>	
+		</div>
+		<div class="form-group">
+			<input type="submit" value="Submit">
+		</div>		
+	</form>
+
+	</div>
+</div>
+
+<div class="article">
+	<h2>
+		Напишите код, который будет спрашивать логин (prompt).<br>
+		Если посетитель вводит «Админ», то спрашивать пароль, если нажал отмена (escape) – выводить «Вход отменён», если вводит что-то другое – «Я вас не знаю».<br>
+		Пароль проверять так. Если введён пароль «Чёрный Властелин», то выводить «Добро пожаловать!», иначе – «Пароль неверен», при отмене – «Вход отменён».
+	</h2>
+
+	<script>
+		/*var userName = prompt('Введите имя пользователя: ', 'username');
+		var userPass;
+		if (userName == 'admin') {
+			alert('Привет, ' + userName);
+			userPass = prompt('Какой пароль?', '***');
+			if (userPass == 'Черный Властелин') {
+				alert('Добро Пожаловать!');
+			} else {
+				alert('Пошел вон!');
+			}
+		} else if (userName == null || userName == '') {
+			alert('Попробуйте еще раз');
+		} else {
+			alert('Обнаружен посторонний!');
+		}
+		*/
+		var result = ( (5 + 1 < 4) ? 'Мало' : 'Много' );
+		console.log(result);
+
+
+		var sec, minute, hour, day, mounth;
+		sec = 1;
+		minute = sec*60;
+		hour = minute*60;
+		console.log(hour);
+		console.log(day = hour*24);
+		console.log(mounth = day*30);
+
+
+
+	</script>
+</div>
+
+<div class="article">
+
+	<h2>Задача 5 (geekbrains.ru)</h2>
+	<p>
+		1. Задать температуру в градусах по Цельсию. Вывести в alert соответствующую температуру в градусах по Фаренгейту. Подсказка: расчет идет по формуле Tf = (9 / 5) * Tc + 32, где Tf — температура по Фаренгейту, Tc — по Цельсию.
+	</p>
+	<script>
+
+		/*console.log('------------------');
+		console.log('Задача 4');
+		console.log('Пункт 1');
+
+
+
+		var Tc = prompt('Введите температуру по Цельсию', 0); // 25 градусов по Цельсию
+		var Tf = (9 / 5) * Tc + 32;
+
+		alert('Температура по Фаренгейту: ' + Tf);*/
+
+	</script>
+
+</div>
+
 <div class='article'>
 	<div>
+
 		<h2>Задача 1</h2>
 		<h5>Составить алгоритм для вычисления значения площади прямоугольника со сторонами a, b.<br>
 		Увеличить полученное значение на 1. Проверить значения при помощи функции console.log(var).</h5>
@@ -17,6 +113,7 @@
 			var a = 5;
 			var b = 6;
 			var c = a*a + b*b + 1;
+			console.log('------------------');
 			console.log('Задача 1');
 			console.log('Площадь прямоугольника равна: ' + c);
 		</script>
--- app/views/issuesphp_view.php
+++ app/views/issuesphp_view.php
@@ -3,96 +3,98 @@
 </div>
 
 <div class="article">
-	
+
 	<h2>Урок 1</h2>
-	
-	<h4>Пункт 1</h4>
-	
-	<p>Вывод разных типов данных</p>
 
-	<p>
-		<?php 
-		$a = 1;
-		$b = 1.2;
-		$c = true;
-		$d = 'string';
-		
-		define('PI' , '3.14');
-		const NEW_PER_PAGE = 10;
+	<div class="div__border-bottom">
+		<h4>Пункт 1</h4>
+		<p>Вывод разных типов данных</p>
+		<p class="monospace">
+			<?php
+$a = 1;
+$b = 1.2;
+$c = true;
+$d = 'string';
 
-		echo 'Целочисленное ' . $a . '<br>';
-		echo 'Дробное ' . $b . '<br>';
-		echo 'Булево ' . $c . '<br>';
-		echo 'Строковое ' . $d . '<br>';
-		echo 'Число Пи, константа ' . PI . '<br>';
-		echo 'Константа ' . NEW_PER_PAGE;
-		?>
-	</p>
+define('PI', '3.14');
+const NEW_PER_PAGE = 10;
 
-	<h4>Пункт 2</h4>
+echo 'Целочисленное ' . $a . '<br>';
+echo 'Дробное ' . $b . '<br>';
+echo 'Булево true ' . $c . '<br>';
+echo 'Строковое ' . $d . '<br>';
+echo 'Число Пи, константа ' . PI . '<br>';
+echo 'Константа ' . NEW_PER_PAGE;
+?>
+		</p>
+	</div>
 
-	<p>Четверостишие на PHP</p>
-
-	<?php 
-		$st1 = '«Славная осень! Здоровый, ядреный';
-		$st2 = 'Воздух усталые силы бодрит;';
-		$st3 = 'Лед неокрепший на речке студеной';
-		$st4 = 'Словно как тающий сахар лежит.»';
-		$st5 = '<u>Н. А. Некрасов</u>';
-
-		echo '<p>' . $st1 . '<br>';
-		echo $st2 . '<br>';
-		echo $st3 . '<br>';
-		echo $st4 . '<br>';
-		echo $st5 . '</p>';
-
-		echo '<p>' . $st1 . '<br>' . $st2 . '<br>' . $st3 . '<br>'. $st4 . '<br>' . $st5 . '</p>';
-	?>
-
-	<h4>Пункт 3</h4>
-
-	<p>Сложение разных типов данных</p>
-
-	<?php 
-		$num = 20;
-		$string = '20 приветов';
-		echo '<p>' . ($num + $string) . '</p>';
-	?>
-
-	<h4>Пункт 4</h4>
-
-	<p>Оператор XOR</p>
-
-	<?php 
-
-	$t = true;
-	$f = false;
-	$result = $t xor $f;
-	echo '<p>' . $result . '</p>';
-
-	?>
-	
-	<h4>Пункт 5</h4>
-
-	<p>x = 10, y = 15. надо поменять местами, не используюя других переменных.</p>
+	<div class="div__border-bottom">
+		<h4>Пункт 2</h4>
+		<p>Четверостишие на PHP</p>
 
 	<?php
-	$x = 10;
-	$y = 15;
+$st1 = '«Славная осень! Здоровый, ядреный';
+$st2 = 'Воздух усталые силы бодрит;';
+$st3 = 'Лед неокрепший на речке студеной';
+$st4 = 'Словно как тающий сахар лежит.»';
+$st5 = '<u>Н. А. Некрасов</u>';
 
-	$y = $y + $x; // 25
-	$x = $y - $x; // 15
-	$y = $y - $x; // 10
-	echo '<p><code>$y = $y + $x; // 25 </code>' . '<br>';
-	echo '<code>$x = $y - $x // 15 </code>' . '<br>';
-	echo '<code>$y = $y - $x; // 10 </code>' . '</p>';
-	?>
+echo '<p class="monospace">' . $st1 . '<br>';
+echo $st2 . '<br>';
+echo $st3 . '<br>';
+echo $st4 . '<br>';
+echo $st5 . '</p>';
+
+echo '<p class="monospace">' . $st1 . '<br>' . $st2 . '<br>' . $st3 . '<br>' . $st4 . '<br>' . $st5 . '</p>';
+?>
+	</div>
+
+	<div class="div__border-bottom">
+		<h4>Пункт 3</h4>
+		<p>Сложение разных типов данных</p>
+
+	<?php
+$num    = 20;
+$string = '20 приветов';
+echo '<p class="monospace"> 20 + 20 приветов = ' . ($num + $string) . '</p>';
+?>
+	</div>
+
+	<div class="div__border-bottom">
+		<h4>Пункт 4</h4>
+		<p>Оператор XOR</p>
+
+		<?php
+$t      = true;
+$f      = false;
+$result = $t xor $f;
+echo '<p class="monospace"> $t xor $f = ' . $result . '</p>';
+?>
+	</div>
+
+	<div class="div__border-bottom">
+		<h4>Пункт 5</h4>
+
+		<p>x = 10, y = 15. надо поменять местами, не используюя других переменных.</p>
+		<?php
+$x = 10;
+$y = 15;
+$y = $y + $x; // 25
+$x = $y - $x; // 15
+$y = $y - $x; // 10
+echo '<p class="monospace">$y = $y + $x; // 25' . '<br>';
+echo '$x = $y - $x // 15' . '<br>';
+echo '$y = $y - $x; // 10' . '</p>';
+?>
+	</div>
 
 </div>
 
 <div class="article">
 	<h2>Урок 2</h2>
-	
+
+	<div class="div__border-bottom">
 	<h4>Пункт 1</h4>
 
 	<p>
@@ -103,88 +105,99 @@
 		Ноль можно считать положительным числом.
 	</p>
 
-	<?php 
-		$a = 10;
-		$b = 15;
+	<?php
+$a = 10;
+$b = 15;
 
-		if ($a > $b) {
-			echo '<p> $a > $b </p>';
-		} else if ($a < $b) {
-			echo '<p> $a < $b </p>';
-		} else {
-			echo '<p> $a = $b </p>';
-		}
+if ($a > $b) {
+    echo '<p class="monospace"> $a > $b </p>';
+} else if ($a < $b) {
+    echo '<p class="monospace"> $a < $b </p>';
+} else {
+    echo '<p class="monospace"> $a = $b </p>';
+}
 
-	?>
+?>
+	</div>
 
+	<div class="div__border-bottom">
 	<h4>Пункт 2</h4>
 
 	<p>
 		Присвоить переменной $а значение в промежутке [0..15]. С помощью оператора switch организовать вывод чисел от $a до 15.
 	</p>
 
-	<p>
+	<p class="monospace">
 		<?php
-		$a = 10;
+$a = 10;
 
-		switch ( $a ) {
-			case '15':
-				echo $a;
-				break;
-			case '14':
-				echo $a;
-				break;
-			case '13':
-				echo $a;
-				break;
-			case '12':
-				echo $a;
-				break;
-			case '11':
-				echo $a;
-				break;
-			case '10':
-				echo $a;
-				break;
-			default:
-				echo $a;
-				break;
-		}
+switch ($a) {
+    case '15':
+        echo $a;
+        break;
+    case '14':
+        echo $a;
+        break;
+    case '13':
+        echo $a;
+        break;
+    case '12':
+        echo $a;
+        break;
+    case '11':
+        echo $a;
+        break;
+    case '10':
+        echo $a;
+        break;
+    default:
+        echo $a;
+        break;
+}
 
-		?>
+?>
 	</p>
+	</div>
 
+	<div class="div__border-bottom">
 	<h4>Пункт 3</h4>
 
 	<p>Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return.</p>
 
-	<?php 
-		function sum($x,$y){
-			$result = $x + $y;
-			return $result;
-		}
-		function mult($x,$y){
-			$result = $x * $y;
-			return $result;	
-		}
-		function sub($x,$y){
-			$result = $x - $y;
-			return $result;
-		}
-		function div($x,$y){
-			$result = $x / $y;
-			return $result;
-		}
-		$sumNum = sum(10,15);
-		$multNum = mult(10,15);
-		$subNum = sub(10,15);
-		$divNum = div(10,15);
-		echo '<p>Сумма: '. $sumNum .'</p>';
-		echo '<p>Умножение: '. $multNum .'</p>';
-		echo '<p>Вычитание: '. $subNum .'</p>';
-		echo '<p>Деление: '. $divNum .'</p>';
-	?>
+	<?php
+function sum($x, $y)
+{
+    $result = $x + $y;
+    return $result;
+}
+function mult($x, $y)
+{
+    $result = $x * $y;
+    return $result;
+}
+function sub($x, $y)
+{
+    $result = $x - $y;
+    return $result;
+}
+function div($x, $y)
+{
+    $result = $x / $y;
+    return $result;
+}
+$sumNum  = sum(10, 15);
+$multNum = mult(10, 15);
+$subNum  = sub(10, 15);
+$divNum  = div(10, 15);
+echo '<p class="monospace">Сумма: ' . $sumNum . '</p>';
+echo '<p class="monospace">Умножение: ' . $multNum . '</p>';
+echo '<p class="monospace">Вычитание: ' . $subNum . '</p>';
+echo '<p class="monospace">Деление: ' . $divNum . '</p>';
+?>
 
+	</div>
+
+	<div class="div__border-bottom">
 	<h4>Пункт 4 *</h4>
 	<p>
 		Реализовать функцию с тремя параметрами: function mathOperation($arg1, $arg2, $operation),
@@ -194,57 +207,140 @@
 	</p>
 
 	<?php
-	function mathOperation($arg1, $arg2, $operation){
-			return;
-	}
-	?>
+function mathOperation($arg1, $arg2, $operation)
+{
 
+    switch ($operation) {
+        case 'addition':
+            $resultOperation = sum($arg1, $arg2);
+            break;
+        case 'subtraction':
+            $resultOperation = sub($arg1, $arg2);
+            break;
+        case 'multiplication':
+            $resultOperation = mult($arg1, $arg2);
+            break;
+        case 'division':
+            $resultOperation = div($arg1, $arg2);
+            break;
+    }
+
+    return $resultOperation;
+}
+
+echo '<p class="monospace"> Сложение 10 + 20 = ' . mathOperation(10, 20, 'addition') . '</p>';
+echo '<p class="monospace"> Вычитание 10 - 20 = ' . mathOperation(10, 20, 'subtraction') . '</p>';
+echo '<p class="monospace"> Умножение 10 * 20 = ' . mathOperation(10, 20, 'multiplication') . '</p>';
+echo '<p class="monospace"> Деление 10 / 20 = ' . mathOperation(10, 20, 'division') . '</p>';
+?>
+	</div>
+
+	<div class="div__border-bottom">
 	<h4>Пункт 5</h4>
 	<p>Посмотреть на встроенные функции PHP. Используя имеющийся HTML шаблон, вывести текущий год в подвале при помощи встроенных функций PHP.</p>
-	<p>
-		<?php echo 'Сейчас ' . date('Y') . ' год';?>
+	<p class="monospace">
+		<?php echo 'Сейчас ' . date('Y') . ' год'; ?>
 	</p>
+</div>
+
+<div class="div__border-bottom">
 
 	<h4>Пункт 6 *</h4>
-	<p>*С помощью рекурсии организовать функцию возведения числа в степень. 
+	<p>*С помощью рекурсии организовать функцию возведения числа в степень. <br>
 	Формат: function power($val, $pow), где $val – заданное число, $pow – степень.</p>
 
+	<?php
+// возведение в степень
+function power($val, $pow)
+{
 
+    if ($pow == 0) {
+        $resultPower = 1; // ну тут понятно
+    } else if ($pow > 0) {
+        $resultPower = $val * power($val, $pow - 1);
+    } else {
+        if ($val == 0) {
+            $resultPower = "Нельзя делить на ноль!";
+        } else {
+            $resultPower = power(1 / $val, -$pow); //
+        }
+
+    }
+    return $resultPower;
+}
+
+echo '<p class="monospace"> Возведем 3 в степень 5, будет ' . power(3, 5) . '</p>';
+echo '<p class="monospace"> Возведем 3 в степень -5, будет ' . power(3, -5) . '</p>';
+?>
+</div>
+
+	<div class="div__border-bottom">
 	<h4>Пункт 7</h4>
 	<p>*Написать функцию, которая вычисляет текущее время и возвращает его в формате с правильными склонениями, например: 22 часа 15 минут, 21 час 43 минуты</p>
-	
+
 	<?php
 
-		function stringCyrData() {
-			$hour = date('H');
-			$minute = date('i');
+function stringCyrData()
+{
+    $hour   = date('H');
+    $minute = date('i');
 
-			if ( $minute == 1 || $minute == 21 || $minute == 31 || $minute == 41 || $minute == 51) {
-				$stMinute = $minute . ' минута';
-			} else if ( ($minute >= 2 && $minute <= 4) || ($minute >= 22 && $minute <= 24) || ($minute >= 32 && $minute <= 34) || ($minute >= 42 && $minute <= 44) || ($minute >= 52 && $minute <= 54) ) {
-				$stMinute = $minute . ' минуты';
-			} else {
-				$stMinute = $minute . ' минут';
-			} 
+    if ($minute == 1 || $minute == 21 || $minute == 31 || $minute == 41 || $minute == 51) {
+        $stMinute = $minute . ' минута';
+    } else if (($minute >= 2 && $minute <= 4) || ($minute >= 22 && $minute <= 24) || ($minute >= 32 && $minute <= 34) || ($minute >= 42 && $minute <= 44) || ($minute >= 52 && $minute <= 54)) {
+        $stMinute = $minute . ' минуты';
+    } else {
+        $stMinute = $minute . ' минут';
+    }
 
-			if ( $hour == 1 || $hour == 21) {
-				$stHour = $hour . ' час';
-			} else if ( $hour >= 2 && $hour <= 4 || $hour >= 22 && $hour <= 24 ) {
-				$stHour = $hour . ' часа';
-			} else {
-				$stHour = $hour . ' часов';
-			}
+    switch ($hour) {
+        case ('1' || '21'):
+            $stHour = "$hour час";
+            break;
+        case ('2' || '3' || '4' || '22' || '23' || '24'):
+            $stHour = "$hour часа";
+            break;
+        default:
+            $stHour = "$hour часов";
+            break;
+    }
 
-			$stData = $stHour . ' ' . $stMinute;
+    $stData = $stHour . ' ' . $stMinute; // ..часов ..минут в кириллице
 
-			return $stData;
-		}
+    return $stData;
+}
 
-		echo '<p>' . stringCyrData() . '</p>'; 
-	?>
-
+echo '<p class="monospace">' . stringCyrData() . '</p>';
+?>
+	</div>
 </div>
 
 <div class="article">
-	<h2>Урок 3</h2>
+	<h2>Проблемы с тестирования</h2>
+	<p>Чему равно выражение?</p>
+	<p>
+		<code>
+			$a = 1;<br>
+			$b = 1;<br>
+			$c = $a + $b++;<br>
+			echo $c;
+		</code>
+	</p>
+	<div>
+		<p>	А равно оно нифига не 3, а <strong>	2</strong></p>
+		<p>
+			<?php
+
+$a = 1;
+echo '$a = ' . $a . '<br>';
+$b = 1;
+echo '$b = ' . $b . '<br>';
+$c = $a + $b++;
+echo '$c = $a + $b++ = ' . $c;
+
+?>
+		</p>
+
+	</div>
+
 </div>
